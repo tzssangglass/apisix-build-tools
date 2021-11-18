48de339c6b927cf9b24563a73ef0fbb8dc3f9d6b
tzssangglass <tzssangglass@gmail.com>
2021-11-18 13:13:26 +0800

    update
    Signed-off-by: tzssangglass <tzssangglass@gmail.com>

--- utils/sign-rpm.sh
+++ utils/sign-rpm.sh
@@ -4,16 +4,16 @@ set -x
 
 
 import_gpg_key() {
-    gpg --import --pinentry-mode loopback --batch --passphrase-file \
+    sudo gpg --import --pinentry-mode loopback --batch --passphrase-file \
     /tmp/rpm-gpg-publish.passphrase /tmp/rpm-gpg-publish.private
 
-    gpg --list-keys --fingerprint | grep "${GPG_MAIL}" -B 1 \
+    sudo gpg --list-keys --fingerprint | grep "${GPG_MAIL}" -B 1 \
     | tr -d ' ' | head -1 | awk 'BEGIN { FS = "\n" } ; { print $1":6:" }' \
     | gpg --import-ownertrust
 }
 
 rpm_sign() {
-    cat > ~/.rpmmacros <<EOF
+    sudo cat > ~/.rpmmacros <<EOF
 # Macros for signing RPMs.
 %_signature gpg
 %_gpg_path ${HOME}/.gnupg
@@ -21,11 +21,11 @@ rpm_sign() {
 %_gpgbin /usr/bin/gpg
 %__gpg_sign_cmd %{__gpg} gpg --batch --verbose --no-armor --pinentry-mode loopback --passphrase-file /tmp/rpm-gpg-publish.passphrase --no-secmem-warning -u "%{_gpg_name}" -sbo %{__signature_filename} --digest-algo sha256 %{__plaintext_filename}
 EOF
-    rpmsign --addsign ./apisix-base-${{ steps.tag_env.outputs.version }}-0.el7.x86_64.rpm
+    sudo rpmsign --addsign ./apisix-base-${{ steps.tag_env.outputs.version }}-0.el7.x86_64.rpm
 }
 
 rpm_checksig() {
-    rpm --import https://repos.apiseven.com/KEYS
+    sudo rpm --import https://repos.apiseven.com/KEYS
 
     out=$(rpm --checksig ./apisix-base-${{ steps.tag_env.outputs.version }}-0.el7.x86_64.rpm)
     if ! echo "$out" | grep "digests signatures OK"; then
