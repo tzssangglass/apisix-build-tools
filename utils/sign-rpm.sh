58092c1764eb151ada12c5b2186b3096992da58f
tzssangglass <tzssangglass@gmail.com>
2021-11-18 15:32:24 +0800

    update
    Signed-off-by: tzssangglass <tzssangglass@gmail.com>

--- utils/sign-rpm.sh
+++ utils/sign-rpm.sh
@@ -21,13 +21,13 @@ rpm_sign() {
 %_gpgbin /usr/bin/gpg
 %__gpg_sign_cmd %{__gpg} gpg --batch --verbose --no-armor --pinentry-mode loopback --passphrase-file /tmp/rpm-gpg-publish.passphrase --no-secmem-warning -u "%{_gpg_name}" -sbo %{__signature_filename} --digest-algo sha256 %{__plaintext_filename}
 EOF
-    rpmsign --addsign ./apisix-base-${{ steps.tag_env.outputs.version }}-0.el7.x86_64.rpm
+    rpmsign --addsign ./apisix-base-${APISIX_BASE_TAG_VERSION}-0.el7.x86_64.rpm
 }
 
 rpm_checksig() {
     rpm --import https://repos.apiseven.com/KEYS
 
-    out=$(rpm --checksig ./apisix-base-${{ steps.tag_env.outputs.version }}-0.el7.x86_64.rpm)
+    out=$(rpm --checksig ./apisix-base-${APISIX_BASE_TAG_VERSION}-0.el7.x86_64.rpm)
     if ! echo "$out" | grep "digests signatures OK"; then
         echo "failed: check rpm digests signatures failure"
         exit 1
