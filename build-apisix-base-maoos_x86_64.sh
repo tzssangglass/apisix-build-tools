3bcf4f5bd5773d11f3bbec006396032edb53e706
tzssangglass <tzssangglass@gmail.com>
2022-01-14 12:04:42 +0800

    update
    Signed-off-by: tzssangglass <tzssangglass@gmail.com>

--- build-apisix-base-maoos_x86_64.sh
+++ build-apisix-base-maoos_x86_64.sh
@@ -6,7 +6,7 @@ export openssl_prefix=/usr/local/Cellar/openresty-openssl111/1.1.1l_1
 export zlib_prefix=/usr/local/Cellar/zlib/1.2.11
 export pcre_prefix=/usr/local/Cellar/pcre/8.45
 
-export cc_opt="-DNGX_LUA_ABORT_AT_PANIC -I${zlib_prefix}/include -I${pcre_prefix}/include -I${openssl_prefix}/include"
+export cc_opt="-DNGX_LUA_ABORT_AT_PANIC -I${zlib_prefix}/include -I${pcre_prefix}/include -I${openssl_prefix}/include -O0"
 export ld_opt="-L${zlib_prefix}/lib -L${pcre_prefix}/lib -L${openssl_prefix}/lib -Wl,-rpath,${zlib_prefix}/lib:${pcre_prefix}/lib:${openssl_prefix}/lib"
 export luajit_xcflags="-DLUAJIT_ASSERT -DLUAJIT_NUMMODE=2 -DLUAJIT_ENABLE_LUA52COMPAT -O0"
 export OR_PREFIX=/usr/local/Cellar/openresty-debug
