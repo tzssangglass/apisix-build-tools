dc801838226740ebfa5276f7b0e41f78d73e10b6
tzssangglass <tzssangglass@gmail.com>
2022-03-11 19:02:13 +0800

    save

--- build-apisix-base-debug-centos7.sh
+++ build-apisix-base-debug-centos7.sh
@@ -4,7 +4,7 @@ set -x
 
 yum-config-manager --add-repo https://openresty.org/package/centos/openresty.repo
 yum -y install gcc gcc-c++ patch wget git make sudo
-yum -y install openresty-openssl111-debug-devel openresty-pcre-devel openresty-zlib-devel
+yum -y install openresty-openssl111-debug-debuginfo openresty-openssl111-debug-debuginfo openresty-openssl111-debug-debuginfo
 
 export openssl_prefix=/usr/local/openresty-debug/openssl111
 export zlib_prefix=/usr/local/openresty/zlib
@@ -12,7 +12,7 @@ export pcre_prefix=/usr/local/openresty/pcre
 
 export cc_opt="-DNGX_LUA_USE_ASSERT -DNGX_LUA_ABORT_AT_PANIC -I${zlib_prefix}/include -I${pcre_prefix}/include -I${openssl_prefix}/include -O0"
 export ld_opt="-L${zlib_prefix}/lib -L${pcre_prefix}/lib -L${openssl_prefix}/lib -Wl,-rpath,${zlib_prefix}/lib:${pcre_prefix}/lib:${openssl_prefix}/lib"
-export luajit_xcflags="-DLUAJIT_ASSERT -DLUAJIT_NUMMODE=2 -DLUAJIT_ENABLE_LUA52COMPAT -O0"
+export luajit_xcflags="-DLUAJIT_ASSERT -DLUAJIT_NUMMODE=2 -DLUAJIT_ENABLE_LUA52COMPAT -O0 -g"
 export OR_PREFIX=/usr/local/openresty-debug
 export debug_args=--with-debug
 
